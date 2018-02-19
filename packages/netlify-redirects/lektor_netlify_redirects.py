5489c2aaa1aaac527bca6511d4553d6d91a03e43
Daniel Evans <migetman9@gmail.com>
2018-02-19 01:43:57 -0800

    remove silly things

--- packages/netlify-redirects/lektor_netlify_redirects.py
+++ packages/netlify-redirects/lektor_netlify_redirects.py
@@ -16,7 +16,7 @@ class CopyPublisher(Publisher):
         myQuery = Query('/', pad)
         items = myQuery.filter(F._model == 'portal')
         output_location = self.output_path
-        #print(output_location)
+
         with open('{}/_redirect'.format(output_location), 'wb') as redirect_file:
             for item in items:
                 for block in item['body'].blocks:
@@ -32,24 +32,13 @@ class CopyPublisher(Publisher):
                     except KeyError as e:
                         yield 'skipping {}'.format(block['title'])
 
-        target_dir = os.getcwd() + target_url.path
-        yield "copying to local directory %s" % target_dir
-
-        # Clear the target directory if it exists
-        yield "clearing target directory"
-        shutil.rmtree(target_dir, ignore_errors=True)
-
-        # Copy the build output to the target directory
-        yield "copying tree"
-        shutil.copytree(self.output_path, target_dir)
-
 
 class NetlifyRedirectsPlugin(Plugin):
     name = u'Netlify Redirects'
     description = u'Automatically creates redirects on netlify'
 
     def on_setup_env(self, **extra):
-        self.env.add_publisher('redirect', CopyPublisher)
+        self.env.add_publisher('copy', CopyPublisher)
 
     def on_process_template_context(self, context, **extra):
         def test_function():
