e9cfa233e931dbf3105a34bea8beddf7815e0e6a
Daniel Evans <migetman9@gmail.com>
2018-02-19 01:35:34 -0800

    maybe it works there

--- packages/netlify-redirects/lektor_netlify_redirects.py
+++ packages/netlify-redirects/lektor_netlify_redirects.py
@@ -15,20 +15,9 @@ class CopyPublisher(Publisher):
         pad = self.env.new_pad()
         myQuery = Query('/', pad)
         items = myQuery.filter(F._model == 'portal')
-
-
-        target_dir = os.getcwd() + target_url.path
-        yield "copying to local directory %s" % target_dir
-
-        # Clear the target directory if it exists
-        yield "clearing target directory"
-        shutil.rmtree(target_dir, ignore_errors=True)
-
-        # Copy the build output to the target directory
-        yield "copying tree"
-        shutil.copytree(self.output_path, target_dir)
-
-        with open('{}/_redirect'.format(target_dir), 'wb') as redirect_file:
+        output_location = self.output_path
+        #print(output_location)
+        with open('{}/_redirect'.format(output_location), 'wb') as redirect_file:
             for item in items:
                 for block in item['body'].blocks:
                     try:
@@ -43,6 +32,16 @@ class CopyPublisher(Publisher):
                     except KeyError as e:
                         yield 'skipping {}'.format(block['title'])
 
+        target_dir = os.getcwd() + target_url.path
+        yield "copying to local directory %s" % target_dir
+
+        # Clear the target directory if it exists
+        yield "clearing target directory"
+        shutil.rmtree(target_dir, ignore_errors=True)
+
+        # Copy the build output to the target directory
+        yield "copying tree"
+        shutil.copytree(self.output_path, target_dir)
 
 
 class NetlifyRedirectsPlugin(Plugin):
@@ -50,9 +49,31 @@ class NetlifyRedirectsPlugin(Plugin):
     description = u'Automatically creates redirects on netlify'
 
     def on_setup_env(self, **extra):
-        self.env.add_publisher('redirect', CopyPublisher)
+        pass
+        #self.env.add_publisher('redirect', CopyPublisher)
 
     def on_process_template_context(self, context, **extra):
         def test_function():
             return 'Value from plugin %s' % self.name
         context['test_function'] = test_function
+
+    def on_after_build_all(self, builder, **options):
+        pad = self.env.new_pad()
+        myQuery = Query('/', pad)
+        items = myQuery.filter(F._model == 'portal')
+        output_location = self.builder.destination_path
+        #print(output_location)
+        with open('{}/_redirect'.format(output_location), 'wb') as redirect_file:
+            for item in items:
+                for block in item['body'].blocks:
+                    try:
+                        status = block['status']
+                    except KeyError as e:
+                        status = ''
+                    try:
+                        slug = quote_plus(block['slug'])
+                        redirect_file.write("{} {} {}".format(
+                        "/{}/{}".format(item['_slug'], slug),
+                        block['url'], status))
+                    except KeyError as e:
+                        yield 'skipping {}'.format(block['title'])
