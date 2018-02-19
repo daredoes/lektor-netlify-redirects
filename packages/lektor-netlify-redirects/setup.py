b2d2dfd51dda4f428bc3323aa24917bd49074687
Daniel Evans <migetman9@gmail.com>
2018-02-19 02:12:10 -0800

    update to v2

--- packages/lektor-netlify-redirects/setup.py
+++ packages/lektor-netlify-redirects/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lektor-netlify-redirects',
-    version='0.1',
+    version='0.2',
     author=u'DareDoes',
     author_email='me@daredoes.work',
     url='https://daredoes.work',
